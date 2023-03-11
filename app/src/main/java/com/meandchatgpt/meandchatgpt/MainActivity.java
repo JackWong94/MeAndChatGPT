773761a027b9f20700139aa99ff3af6d6606c56b
JackWong94 <w.yuheng94@gmail.com>
2023-03-11 08:46:12 +0800

    Initial commit

---
+++ app/src/main/java/com/meandchatgpt/meandchatgpt/MainActivity.java
@@ -0,0 +1,54 @@
+package com.meandchatgpt.meandchatgpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.*;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://chat.openai.com/chat");
+        String defaultUserAgent = System.getProperty("http.agent");
+        webView.getSettings().setUserAgentString(defaultUserAgent);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (webView != null) {
+            webView.onPause();
+            webView.pauseTimers();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (webView != null) {
+            webView.onResume();
+            webView.resumeTimers();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webView != null) {
+            webView.destroy();
+            webView = null;
+        }
+    }
+}
